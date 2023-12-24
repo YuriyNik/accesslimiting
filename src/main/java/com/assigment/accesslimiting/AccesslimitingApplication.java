d6eb013e2d99296453bb475652ab04f2d40893c2
YuriyNik <yuriy.nikitenko@gmail.com>
2023-12-24 15:35:28 +0200

    init

---
+++ src/main/java/com/assigment/accesslimiting/AccesslimitingApplication.java
@@ -0,0 +1,13 @@
+package com.assigment.accesslimiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesslimitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesslimitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assigment/accesslimiting/AccesslimitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment.accesslimiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesslimitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
